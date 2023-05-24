bcfe9e22ab0c33935a2ca40b67c0e7aa9fb4da07
MartinAnzaldop <martinanzaldo1999@gmail.com>
2023-05-24 16:45:32 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const conectarDB=require('./config/db')
+const cors = require('cors');
+const app=express();
+//conectar a la base de datos
+conectarDB()
+app.use(cors());
+app.use(express.json());
+//Funciones de Martin
+
+
+
+
+//Funciones de Ale
+
+
+
+
+
+//Funciones de Norma
+
+
+
+
+//Funciones de Ana
+app.listen(4000, ()=>{
+    console.log('El servidor 4000 esta corriendo')
+})
