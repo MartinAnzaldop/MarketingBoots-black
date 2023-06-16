ff1d290778604f7a06c6a4c37f6a14f0dfc05ed8
NormaNavidadHernandez <norma_mor_25@hotmail.com>
2023-06-16 00:20:10 -0600

    Se agregaron las apis pos, get, put, y delete

--- routes/encuestador.js
+++ routes/encuestador.js
@@ -2,3 +2,12 @@
 const express = require('express');
 const router = express.Router();
 const encuestadorController = require('../controller/encuestadorController');
+
+//api/encuestador
+router.post('/', encuestadorController.crearEncuestador);
+router.get('/', encuestadorController.consultarEncuestador);
+router.get('/:id', encuestadorController.consultarEncuestadorById);
+router.put('/:id', encuestadorController.editarEncuestador);
+router.delete('/:id', encuestadorController.eliminarEncuestador);
+
+module.exports = router;
