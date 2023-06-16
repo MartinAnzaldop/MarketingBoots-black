81773186faa5be98ef4a9ad569ecc9e0a64cde92
NormaNavidadHernandez <norma_mor_25@hotmail.com>
2023-06-16 00:25:25 -0600

    Metodo get consultar por id al encuestador

--- controller/encuestadorController.js
+++ controller/encuestadorController.js
@@ -11,3 +11,26 @@ exports.crearEncuestador= async (req, res)=>{
     res.status(500).send('Error al agregar un encuestador');
  }
 }
+
+//Get//
+exports.consultarEncuestador= async (req, res)=>{
+    try {
+        const encuestador = await Encuestador.find();
+        res.json(encuestador)
+    } catch (error) {
+       res.status(500).send('Error al consultar al encuestador');
+    }
+   }
+
+//Peticion id//
+exports.consultarEncuestadorById= async (req, res)=>{
+    try {
+        let encuestador = await Encuestador.findById(req.params.id);
+        if(!encuestador){
+            res.status(404).json({ msg: "No existe el encuestador con ese id"})
+           }
+           res.json(encuestador);
+    } catch{ 
+       res.status(500).send('Error al consultar el encuestador por id');
+    }
+   }  
