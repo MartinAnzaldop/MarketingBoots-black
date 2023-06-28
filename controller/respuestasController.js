eed95583130e7a87ac2a37f7d951807c4b62e95d
NormaNavidadHernandez <norma_mor_25@hotmail.com>
2023-06-28 16:58:53 -0600

    Se realizaron cambios en el controller

--- controller/respuestasController.js
+++ controller/respuestasController.js
@@ -51,4 +51,44 @@ exports.consultarRespuestas= async (req, res)=>{
     }
    };
 
+//Peticion Put//
+exports.editarRespuestas = async (req, res)=>{
+  try {
+     const {respuesta1, respuesta2, respuesta3, respuesta4, respuesta5,
+      respuesta6, respuesta7, respuesta8, respuesta9, respuesta10}=req.body;
+     let respuestas = await Respuestas.findById(req.params.id);
+     if(!respuestas){
+      res.status(404).json({ msg: "No existe la respuesta con esa id"})
+     }
+     respuestas.respuesta1 = respuesta1;
+     respuestas.respuesta2 = respuesta2;
+     respuestas.respuesta3 = respuesta3;
+     respuestas.respuesta4 = respuesta4;
+     respuestas.respuesta5 = respuesta5;
+     respuestas.respuesta6 = respuesta6;
+     respuestas.respuesta7 = respuesta7;
+     respuestas.respuesta8 = respuesta8;
+     respuestas.respuesta9 = respuesta9;
+     respuestas.respuesta10 = respuesta10,
+     
+     respuestas = await Respuestas.findOneAndUpdate({_id: req.params.id}, respuestas, {new:true})
+     res.json(respuestas);
+  } catch (error) {
+     res.status(500).send('Error al editar una respuesta');
+  }
+};
 
+
+//Peticion Delete//
+exports.eliminarRespuestas= async (req, res)=>{
+  try {
+      let respuestas = await Respuestas.findById(req.params.id);
+      if(!respuestas){
+          res.status(404).json({ msg: "No existe esta respuesta con id"})
+         }
+         await Respuesta.findByIdAndRemove({_id: req.params.id})
+         res.json({msg:"Respuesta eliminada con exito"});
+  } catch{
+     res.status(500).send('Error al eliminar una respuesta');
+  }
+};
